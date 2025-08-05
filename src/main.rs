7841be3eb24b5d5f26c947de51cfc81ecad5361d
KonstantinPogosskiy <darcus666@yandex.ru>
2025-08-06 01:52:13 +0300

    add part 1 of day 2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::collections::HashMap;
 use std::fs;
 use std::io;
 
-fn calculate_difference(list: &str) {
+fn part_1_day_1() -> Result<(), Box<dyn std::error::Error>> {
+    let list = fs::read_to_string("src/day_1.txt")?;
     let mut left_side = Vec::new();
     let mut right_side = Vec::new();
 
@@ -18,10 +19,13 @@ fn calculate_difference(list: &str) {
 
     let result: i32 = left_side.iter().zip(right_side.iter()).map(|(left, right)| (right - left).abs()).sum();
     println!("{}", result);
+    Ok(())
 }
 
 
-fn calculate_difference_2(list: &str) {
+fn part_2_day_1() -> Result<(), Box<dyn std::error::Error>> {
+    let list = fs::read_to_string("src/day_1.txt")?;
+
     let mut left_side = Vec::new();
     let mut right_side = Vec::new();
 
@@ -36,19 +40,55 @@ fn calculate_difference_2(list: &str) {
     }
 
     let mut right_counts = HashMap::new();
-    for num in right_side {
+    for num in &right_side {
         *right_counts.entry(num).or_insert(0) += 1;
     }
     let total: i32 = left_side.iter()
         .map(|num| {
-            right_counts.get(num).unwrap_or(&0) * num}).sum();
+            right_counts.get(num).unwrap_or(&0) * num
+        })
+        .sum();
 
     println!("{}", total);
+    Ok(())
+}
+
+fn part_1_day_2() -> Result<(), Box<dyn std::error::Error>> {
+    let list = fs::read_to_string("src/day_2.txt")?;
+    let array = list.lines()
+        .map(|arr| {
+            arr.split_whitespace()
+                .filter_map(|item| item.parse::<i32>().ok())
+                .collect::<Vec<i32>>()
+        })
+        .filter(|arr| {
+            let steps_array: Vec<i32> = 
+            //сначала написал вариант с zip, он рабочий но тумач для этого, так как создает два итератора
+
+            // arr.iter().zip(arr.iter().skip(1))
+            //     .map(|(a, b)| b - a)
+
+            //вариант с windows создает окно с парами элементов и делает сдвиг на 1
+                arr.windows(2)
+                .map(|pair| pair[1] - pair[0])
+                .collect();
+            let direction = steps_array[0].signum();
+            steps_array.iter().all(|&step| {
+                step.abs() >= 1 && step.abs() <= 3 && step.signum() == direction
+            })
+        })
+
+        //.len() есть только у коллекциий, а можно посчитать длину элементов самого итератора .count() без создания в памяти коллекции
+        .count();
+        // .collect::<Vec<_>>().len();
+
+    println!("{}", array);
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
-        let data = fs::read_to_string("src/coordinate.txt")?;
-        calculate_difference(&data);
-        calculate_difference_2(&data);
+        part_1_day_1();
+        part_2_day_1();
+        part_1_day_2();
         Ok(())
 }
